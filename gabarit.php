e71abe5abe28aaae98a71bb72a4ec2375c54537e
HugoGEORGET <hugogeorget1994@gmail.com>
2014-12-09 16:38:27 +0100

    Début
    Il manque encore des fichiers pour que ça marche

---
+++ gabarit.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="styles/style.css" />
+	<link href="http://fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css" />
+        <title>
+            <?php echo($titre); ?>
+        </title>
+    </head>
+<?php include 'Vue/header.php' ?>
+
+        <div id="global">
+            
+        </div>
+<?php include 'Vue/footer.php'
+</html>
